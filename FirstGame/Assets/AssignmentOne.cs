6df7b35bbd4c3aa1c8f31e833034b73df3aadde0
Tracy Paz <tracypaz15@gmail.com>
2018-01-16 11:33:32 -0700

    Added Unity Assets and Assignment scripts

---
+++ FirstGame/Assets/AssignmentOne.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class is an object classification
+//You must have open and closed {} per object
+public class AssignmentOne : CoconutMonkey {
+
+//void is a function that is void or empty or null
+	// Use this for initialization
+	void Racing () {
+		
+	}
+	
+	// Update is called once per frame
+	void Sliping () {
+		
+	}
+}
