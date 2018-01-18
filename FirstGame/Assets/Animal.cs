e287c1583e80cd0d503091fc182d723f3519006b
Tracy Paz <tracypaz15@gmail.com>
2018-01-18 11:16:11 -0700

    Added animal script and tiger assests

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	//this declares what start will do
+	void Start () {
+		print("My animal is eating.");
+		print("Now my animal is sleeping.");
+		print("I love my cat");
+		print("oiejlsijf");
+	}
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
