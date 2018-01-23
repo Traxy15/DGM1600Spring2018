073ca5d1cade532eeebfe0137d6016bd0451dd07
Tracy Paz <tracypaz15@gmail.com>
2018-01-23 13:17:20 -0700

    Added Conventions and Syntax HW

---
+++ FirstGame/Assets/ConvSyntax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvSyntax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+//Syntax and Conventions is basically the language that is used to compute actions.
+//for example 1 we use a ; at the end of functions to convey a stop or in english a "."
+//for example 2 we use "quotation marks" to start a string a of letters to form sentences.
+//for example 3 we use the covention "print" to tell the computing system to show type on screen.
+// Example 4 we use curly braces to begin and end code. Since lines of code can be written within each other, indentation is strongly suggested for mapping purposes.
+// Last example is the forward slashes (2) for comments. These comments will be ignore by computing systems so you can type whatever you need to.
