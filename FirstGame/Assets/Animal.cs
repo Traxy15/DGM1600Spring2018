96399f65ae8c22019c8f979e8f16199b97df7502
Tracy Paz <tracypaz15@gmail.com>
2018-01-30 10:58:16 -0700

    New Public Bools and If Statements added
    Removed class creation assignments, deleted previous if statements, added new if statements.

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -4,43 +4,19 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour {
 
-public string Name;
-
-public int FoodCount;
-
+	public bool Key = false;
+	public bool Inventory = false;
+	public bool 
 
 	void OnMouseDown()
 	{
-		if (1 + 2 == 3)
-		{
-		print(3);	
+		if(Key == true){
+			print ("Abrakadabra");
 		}
 
-		if (FoodCount > 10)
-		{
-			print("The pet has food.");
+		if(Inventory == true){
+			print ("You've collected the gas can");
 		}
-
-		if (FoodCount < 5){
-			print("your pet is low on food.");
-		}
-
-		if (FoodCount!= 0){
-			print("your pet is dead.");
-		}
-
-		if (Name == "Dog"){
-			print("Woof");
-		}
-
-		if (Name == "pet"){
-			print("Yipee");
-		}
-	
-		if (5 * 2 == 10){
-			print("no it's not");
-		}
-		
 	}
 
 	// Update is called once per frame
