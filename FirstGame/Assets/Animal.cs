c91f8ab3e1a7a7c94235179bf572aab90b62e337
Tracy Paz <tracypaz15@gmail.com>
2018-01-30 10:48:14 -0700

    If Statements

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour {
 
-	// Use this for initialization
-	//this declares what start will do
-	void Start () {
-		print("My animal is eating.");
-		print("Now my animal is sleeping.");
-		print("I love my cat");
-		print("oiejlsijf");
-		print("I'm a silly princess");
-	}
+public string Name;
+
+public int FoodCount;
+
+
 	void OnMouseDown()
 	{
-		print("Meow.");
+		if (1 + 2 == 3)
+		{
+		print(3);	
+		}
+
+		if (FoodCount > 10)
+		{
+			print("The pet has food.");
+		}
+
+		if (FoodCount < 5){
+			print("your pet is low on food.");
+		}
+
+		if (FoodCount!= 0){
+			print("your pet is dead.");
+		}
+
+		if (Name == "Dog"){
+			print("Woof");
+		}
+
+		if (Name == "pet"){
+			print("Yipee");
+		}
+	
+		if (5 * 2 == 10){
+			print("no it's not");
+		}
+		
 	}
 
 	// Update is called once per frame
