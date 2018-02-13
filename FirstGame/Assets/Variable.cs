09edd418397a5f18ecd0622f0e09598115c9c340
Tracy Paz <tracypaz15@gmail.com>
2018-02-12 21:34:24 -0700

    Variable Homework

---
+++ FirstGame/Assets/Variable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable : MonoBehaviour {
+// Variables are holders for pieces of information and are unique to each different piece of info. Variables are changing. They are varried from what they do and what information they hold. 
+
+//in this nexzt example the word HomeWork will be the variable.
+
+public int HomeWork;
+//I can end the variable with the semi colon
+
+public int HomeWork = 10;
+//or I can give it a value. 
+
+int PowerUp = 20;
+int Boost;
+int FlatTire = -5;
+
+}
