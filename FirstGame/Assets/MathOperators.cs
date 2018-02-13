20e1c01f9c35b487ae4ea2c4d68bef35e4fb5da7
Tracy Paz <tracypaz15@gmail.com>
2018-02-12 21:20:29 -0700

    Math Operators HW

---
+++ FirstGame/Assets/MathOperators.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUp : ScriptableObject 
+{
+
+	public int PowerLevel = 10;
+	public Player CurrentPlayer;
+
+	public void RunPowerUp()
+	{
+		CurrentPlayer.Score += PowerLevel;
+	}	
+	
+}
+	
