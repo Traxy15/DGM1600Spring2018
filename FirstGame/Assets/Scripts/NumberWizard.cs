5335a36c39aac3841303258d7c618add78e50ed0
Tracy Paz <tracypaz15@gmail.com>
2018-03-13 10:52:08 -0600

    Practicing "IF" statements and  GetKeyDown Command

--- FirstGame/Assets/Scripts/NumberWizard.cs
+++ FirstGame/Assets/Scripts/NumberWizard.cs
@@ -14,10 +14,25 @@ public class NumberWizard : MonoBehaviour {
 
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
+
+		print ("Is the number higher or lower than 500?");
+		print ("Up = higher, Down = lower, Return = equal");
+
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			Debug.Log("Up arrow pressed");
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			Debug.Log("Down arrow pressed");
+		}
+
+		
 		
 	}
 }
