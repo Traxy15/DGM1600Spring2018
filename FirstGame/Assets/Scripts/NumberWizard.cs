8e94a3226ddbc6fc5cc7994eb67b4138a1a7e072
Tracy Paz <tracypaz15@gmail.com>
2018-03-13 12:10:49 -0600

    Practicing Variables and MATHS!!

--- FirstGame/Assets/Scripts/NumberWizard.cs
+++ FirstGame/Assets/Scripts/NumberWizard.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
+	
+	// Use this for initialization	
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
 
-	// Use this for initialization
 	void Start () {
+		max = max + 1;
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me.");
-
-		int max = 1000;
-		int min = 1;
 
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 
-		print ("Is the number higher or lower than 500?");
+		print ("Is the number higher or lower than " + guess +"?");
 		print ("Up = higher, Down = lower, Return = equal");
 
 	
@@ -25,11 +27,17 @@ public class NumberWizard : MonoBehaviour {
 	void Update () {
 
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			Debug.Log("Up arrow pressed");
+			print("Up arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print("Higher or lower than" + guess);
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			Debug.Log("Down arrow pressed");
+			print("Down arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print("Higher or lower than" + guess);
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			Debug.Log("I win!");
+			print("I win!");
 		}
 
 		
