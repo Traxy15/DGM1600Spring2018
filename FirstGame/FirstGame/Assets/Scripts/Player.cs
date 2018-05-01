81fbd974ad9fa0798693cc0eae5d96e51cdf37c6
Tracy Paz <tracypaz15@gmail.com>
2018-05-01 16:21:44 -0600

    Left and Right Movement Script

--- FirstGame/FirstGame/Assets/Scripts/Player.cs
+++ FirstGame/FirstGame/Assets/Scripts/Player.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+Rigidbody2D myRigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+		myRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Run();
+	}
+
+	private void Run() {
 		
+		float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
+		Vector2 playerVelocity = new Vector2(controlThrow, myRigidBody.velocity.y);
+		myRigidBody.velocity = playerVelocity;
 	}
 }
