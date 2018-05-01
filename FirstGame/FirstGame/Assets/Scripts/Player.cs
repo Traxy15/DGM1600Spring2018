74971e19bae5058d8b2f1ea0f956ba28116038d6
Tracy Paz <tracypaz15@gmail.com>
2018-05-01 17:39:13 -0600

    Gravity Settings, Jump Key added

--- FirstGame/FirstGame/Assets/Scripts/Player.cs
+++ FirstGame/FirstGame/Assets/Scripts/Player.cs
@@ -5,19 +5,28 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+	// Config
 	[SerializeField] float runSpeed = 5f;
+	[SerializeField] float jumpSpeed = 5f;
 
+// State
+bool isAlive = true;
+
+// Cached component references
 Rigidbody2D myRigidBody;
+Animator myAnimator;
 
-	// Use this for initialization
+	// message then methods
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Run();
 		FlipSprite();
+		Jump();
 	}
 
 	private void Run() {
@@ -25,6 +34,18 @@ Rigidbody2D myRigidBody;
 		float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
 		Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
 		myRigidBody.velocity = playerVelocity;
+	
+		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+		myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+	}
+
+	private void Jump()
+	{
+		if (CrossPlatformInputManager.GetButtonDown("Jump"))
+		{
+			Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+			myRigidBody.velocity += jumpVelocityToAdd;
+		}
 	}
 
 	private void FlipSprite()
@@ -33,8 +54,9 @@ Rigidbody2D myRigidBody;
 		bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
 		if (playerHasHorizontalSpeed)
 		{ 
-			transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f);
 			//reverse the current scaling of x axis
+			transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f);
+			
 		}
 	}
 }
