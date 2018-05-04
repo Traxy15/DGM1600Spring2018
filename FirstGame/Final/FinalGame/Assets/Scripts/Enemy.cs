35ed2ab78f3dd145d36f56b9ee08411a903d9118
Tracy Paz <tracypaz15@gmail.com>
2018-05-03 21:38:08 -0600

    Enemies explode upon particle collision

--- FirstGame/Final/FinalGame/Assets/Scripts/Enemy.cs
+++ FirstGame/Final/FinalGame/Assets/Scripts/Enemy.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	[SerializeField] GameObject deathFX;
+	[SerializeField] Transform parent;
+
 	// Use this for initialization
 	void Start () {
-		
+		AddNonTriggerBoxCollider();
+	}
+
+	private void AddNonTriggerBoxCollider()
+	{
+		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+		boxCollider.isTrigger = false;
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	void OnParticleCollision(GameObject other)
 	{
+		GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+		fx.transform.parent = parent;
 		Destroy(gameObject);
 	}
 }
