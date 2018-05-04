5f07971a7dff6847dea6220198999ee95e31d3af
Tracy Paz <tracypaz15@gmail.com>
2018-05-03 22:54:26 -0600

    Branching

--- FirstGame/Final/FinalGame/Assets/Scripts/Enemy.cs
+++ FirstGame/Final/FinalGame/Assets/Scripts/Enemy.cs
@@ -6,13 +6,17 @@ public class Enemy : MonoBehaviour {
 
 	[SerializeField] GameObject deathFX;
 	[SerializeField] Transform parent;
+	[SerializeField] int scorePerHit = 12;
+
+	ScoreBoard scoreBoard;
 
 	// Use this for initialization
 	void Start () {
-		AddNonTriggerBoxCollider();
+		AddBoxCollider();
+		scoreBoard = FindObjectOfType<ScoreBoard>();
 	}
 
-	private void AddNonTriggerBoxCollider()
+	private void AddBoxCollider()
 	{
 		Collider boxCollider = gameObject.AddComponent<BoxCollider>();
 		boxCollider.isTrigger = false;
@@ -22,6 +26,7 @@ public class Enemy : MonoBehaviour {
 
 	void OnParticleCollision(GameObject other)
 	{
+		scoreBoard.ScoreHit(scorePerHit);
 		GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
 		fx.transform.parent = parent;
 		Destroy(gameObject);
